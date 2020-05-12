4e18f3415744f5b6082c24f2c11c5f831bb65ef9
kangcy28 <43030079+kangcy28@users.noreply.github.com>
2020-05-12 12:05:10 +0800

    largange complete

--- hw3.py
+++ hw3.py
@@ -11,38 +11,37 @@ def make_point():
     k = 0
     i = 0
     while i <= 25:
-        x.append(2.5 + 0.2 * i)
-        print(2.5+0.2*i)
-        y.append(polynomial(x[k]))
+        x.append(round(2.50 + 0.20 * i, 1))
+        y.append(round(polynomial(x[k]),3))
         i += 1
         k += 1
-
-    #print(y[0:len(y)])
     
-
-
-def lagrange(num):
-	ans = 0
-	for i in range(0,25):
-		s = 0
-		for j in range(0,25):
-			if i==j:
-				continue
-			s = s * (num-x[j]) / (x[i] - x[j])
-		ans += s
-	return ans
-
+def lagrange(num):  
+    ans = 0
+    for i in range(0,len(x)-1):
+        s = 1
+        s *= y[i]
+        for j in range(0,len(y)-1):
+            if i==j:
+                continue
+            s *= (num-x[j]) / (x[i] - x[j])
+        ans += s
+    return round(ans,6)
 f = []
 
 def lagrange_drawgraph():
     k = 0
     i = 2.5
+    #print(lagrange(2.5))
+    #print(lagrange(7.5))
+
     while i <= 7.5:
         f.append(i) 
         L.append(lagrange(i))
         k += 1
         i += 0.001
-
+        #print(f[-1])
+        #print(L[-1])
     plt.xlabel('x - axis')
     plt.ylabel('y - axis')
     plt.title('Lagrange')
@@ -51,7 +50,8 @@ def lagrange_drawgraph():
 
 def main():
     make_point()
-    #lagrange_drawgraph()
+    lagrange_drawgraph()
+
 
 if __name__ == "__main__":
     main()
